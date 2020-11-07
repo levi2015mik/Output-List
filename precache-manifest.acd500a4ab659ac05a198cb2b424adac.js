94a1ab971173b567c5275147a3e7ed8b6d2dc5d9
levi2015mik <55503780+levi2015mik@users.noreply.github.com>
2020-11-07 20:10:57 +0200

    Add files via upload

---
+++ precache-manifest.acd500a4ab659ac05a198cb2b424adac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31960a9dc081a6c64ca437d4cbe5535",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cd53e9fdbad438447a7",
+    "url": "/static/js/2.2ff4dea6.chunk.js"
+  },
+  {
+    "revision": "6c832311cae1bb14ec1bc62c7ebffcc0",
+    "url": "/static/js/2.2ff4dea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6925f6d7146618100fe",
+    "url": "/static/js/main.5895914a.chunk.js"
+  },
+  {
+    "revision": "fface87ac866deef2078",
+    "url": "/static/js/runtime-main.c92c4ed2.js"
+  }
+]);
